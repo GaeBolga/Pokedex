5e919156a5937cdb0b1e15a51972df6013b66652
GaeBolga <benjamin.lefort@mail.com>
2024-10-23 16:01:21 +0200

    création de props

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,26 @@ import PokemonCard from "./components/PokemonCard";
 
   function App() {
 
+   const pokemonList = [
+    { name: "bulbasaur", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" },
+    { name: "mew" },
+   ];
+
+   const {name, imgSrc} = pokemonList[0]
+    
     return (
   
       <section>
         <MyTitle/>
-        <PokemonCard/>
-  
-      </section>
+        <PokemonCard  name="bulbasaur"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+        <PokemonCard  name="mew"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png"/>
+        <PokemonCard  name="mewtwo"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png"/>
+        <PokemonCard  name="salameche"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"/>
+        <PokemonCard  name="??"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/428.png"/>
+        <PokemonCard  name="mew"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/429.png"/>
+        <PokemonCard  name="mew"  imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/430.png"/>
+        <PokemonCard  name={name} imgSrc={imgSrc} />
+        </section>
   
     );
   
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,25 +1,11 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-
-  },
-
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-
-const pokemon = pokemonList[1] ;
-
+function PokemonCard(props : {name: string, imgSrc: string} ){
   return (
     <figure>
-{pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}  
-  </figure>
+      {props.imgSrc ? <img src={props.imgSrc} /> : <p>???</p>}
+      <figcaption>
+        <p>{props.name}</p>
+      </figcaption>
+    </figure>
   );
 }
 
