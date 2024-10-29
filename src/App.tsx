01e5e239a86166d7e0593bfff6b76c6441e6ebd2
GaeBolga <benjamin.lefort@mail.com>
2024-10-29 17:24:01 +0100

    pika pika

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import MyTitle from "./assets/components/MyTitle";
 import NavBar from "./components/NavBar";
 
 function App() {
-
-
   return (
     <section>
+      
       <MyTitle />
 
       <div>
         <NavBar />
-
       </div>
     </section>
   );
 }
+alert("hello pokemon trainer");
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,9 +1,20 @@
+import  { useEffect } from "react";
+
 function PokemonCard(props: { name: string; imgSrc: string }) {
+
+  useEffect(() => {
+    if (props.name === "pikachu") {
+      alert("pika pika !");
+    }
+  }, [props.name]);
+
+
   return (
     <figure>
       {props.imgSrc ? <img src={props.imgSrc} /> : <p>???</p>}
       <figcaption>
         <p>{props.name}</p>
+        
       </figcaption>
     </figure>
   );
